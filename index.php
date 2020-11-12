43f64d79cf155c8de63590bf1b384fd1c9067f1a
vita_lin <vitalinchia@gmail.com>
2020-11-12 14:49:44 +0800

    新增對獎首頁畫面

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>統一發票紀錄及對獎系統</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" ></script> -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+
 </head>
 <body>
+
+    <?php 
+        include "main.php";
+        ?>
+
+
 
 </body>
 </html>
---
+++ main.php
@@ -0,0 +1,62 @@
+<h3 class="text-center mt-5">統一發票記錄與對獎</h3>
+
+<div class="container pt-5">
+    <div class="col-8 d-flex justify-content-between mx-auto p-3 border">
+   
+    <?php 
+
+        $month=[
+                    1=>"1,2月",
+                    2=>"3,4月",
+                    3=>"5,6月",
+                    4=>"7,8月",
+                    5=>"9,10月",
+                    6=>"11,12月"
+        ];
+
+            $m=ceil(date("m")/2);
+    ?>
+   
+   
+   
+    <div class="text-center"><?=$month[$m];?></div>
+    <div class="text-center">
+        <a href="#">當期發票</a>
+    </div>
+    <div class="text-center">
+        
+        <a href="#">對獎</a>
+    </div>
+    <div class="text-center">
+        
+        <a href="#">輸入獎號</a>
+</div>
+    </div>
+</div>
+
+    <div class="col-5 d-flex justify-content-between mx-auto p-3 border">
+    <form action="api/add_invoice.php" method="GET">  <!--action:要指向的檔案-->
+    <div class="pt-3">年份：<input type="text" name="year" ></div>  <!--年份-->
+    <div class="pt-3">   期別：<select name="period" id="">
+            <option value="1">1,2月</option>
+            <option value="2">3,4月</option>
+            <option value="3">5,6月</option>
+            <option value="4">7,8月</option>
+            <option value="5">9,10月</option>
+            <option value="6">11,12月</option>
+        </select></div>
+        <div class="pt-3">發票號碼：
+            <input type="text" name="prepend"  style="width: 50px;">
+            <input type="text" name="number" style="width: 100px;">
+        <div class="text-center pt-3">發票金額：
+        <input type="number" name="payment" style="width: 150px;">
+
+        </div>
+
+        </div>
+
+
+    </form>
+    </div>
+
+</div>
