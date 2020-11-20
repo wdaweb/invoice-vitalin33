c531ff873d3bde09709a7e32bc926f4491ac4d93
vita_lin <vitalinchia@gmail.com>
2020-11-20 10:25:39 +0800

    讓text.php的function可以查詢的形態增加，但變數不增加，只增加判斷式

--- test.php
+++ test.php
@@ -13,15 +13,49 @@
     */
 
 
-    function find($table,$def){  //本語法預設只會取回一筆資料
-        global $pdo;
-        $sql="select * from $table where $def";
-        $row=$pdo->query($sql)->fetch();
-
-        return $row;
-
-
+    /**第一階段：最簡單的函式套用
+    *function find($table,$def){  //本語法預設只會取回一筆資料
+    *    global $pdo;
+    *    $sql="select * from $table where $def";
+    *    $row=$pdo->query($sql)->fetch();
+    *    return $row;
     }
+    */
+
+    
+
+    /**第二階段：進階函式套用
+    /*↓↓↓↓↓↓↓↓在一個function裡同時放數字也可放條件，讓使用範圍更加廣：增加條件判斷式↓↓↓↓↓↓↓*/
+    function find($table,$id){  //本語法預設只會取回一筆資料
+            global $pdo;
+            if(is_numeric($id)){
+            $sql="select * from $table where `id`='$id'"; //如果是數字的話就用這一段
+            }else{
+            $sql="select * from $table where $id";  //如果輸入的不是數字的話就用這一段       
+            }
+            $row=$pdo->query($sql)->fetch();
+            return $row;
+        }
+
+
+
+     $row=find('invoices',"code='AB' && number='69874947'");
+     echo $row['code'].$row['number'];
+     
+     $row=find('invoices',16);
+     echo $row['code'].$row['number'];
+     
+     $row=find('invoices',33);
+     echo $row['code'].$row['number'];
+
+
+
+
+
+
+
+
+
 
 
 
